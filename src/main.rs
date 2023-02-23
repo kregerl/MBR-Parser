13aeaf50e13e46a01c74a04dbbb1cea9f56aec12
kregerl <loucaskreger@gmail.com>
2023-02-23 13:39:43 -0500

    Added commandline options

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+use clap::Parser;
 use mbr::{parse_sector, PartitionTableNode};
 use std::path::Path;
 
 mod mbr;
 
+#[derive(Debug, Parser)]
+struct Arguments {
+    image_path: String,
+}
+
 fn main() {
+    let args = Arguments::parse();
     let mut root = PartitionTableNode::default();
-    if let Err(e) = parse_sector(&mut root, &Path::new("./mbr_test.dd"), 0) {
+    if let Err(e) = parse_sector(&mut root, &Path::new(&args.image_path), 0) {
         eprintln!("Error parsing MBR: {}", e);
     }
     if let Some(children) = root.children {
